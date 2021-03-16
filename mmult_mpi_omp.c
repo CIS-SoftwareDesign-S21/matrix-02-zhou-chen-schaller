2f5fc4b7badad4fced9022a9dba8ee51bd4f0d67
Darren Chen <tuj72544@temple.edu>
2021-03-16 02:19:01 -0400

    Change 3

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -80,12 +80,13 @@ int main(int argc, char *argv[])
 		}
 		fclose(fp);
 	
+		aa = (double *)malloc(sizeof(double) * nrows_1 * ncols_1);
+		bb = (double *)malloc(sizeof(double) * nrows_2 * ncols_2);
+		aa = read_matrix_from_file(argv[1]);
+		bb = read_matrix_from_file(argv[2]);
+
 		if (myid == master)
 		{
-			aa = (double *)malloc(sizeof(double) * nrows_1 * ncols_1);
-			bb = (double *)malloc(sizeof(double) * nrows_2 * ncols_2);
-			aa = read_matrix_from_file(argv[1]);
-			bb = read_matrix_from_file(argv[2]);
 			cc1 = malloc(sizeof(double) * nrows_1 * ncols_2);
         	buffer = (double *)malloc(sizeof(double) * ncols_2);
         	ans = (double *)malloc(sizeof(double) * ncols_2);
