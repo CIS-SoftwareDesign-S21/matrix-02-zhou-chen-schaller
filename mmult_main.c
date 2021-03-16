a3e0897c260e5d711056825d130b081dff7742b9
Ricky Zhou <zhouricky93@gmail.com>
2021-03-15 22:35:24 -0400

    Integrated openMP into mmult_main
    Created mmult_simd_O3 to be able to run it with the -O3 flag
    Altered Makefile accordingly

--- mmult_main.c
+++ mmult_main.c
@@ -17,7 +17,7 @@ int main(void)
 	
 	while(run1)
 	{
-		printf("\nDo you want to clear mmult.txt, mmult_simd.txt, and mmult_simd_O3.txt (if they exist)?\n"
+		printf("\nDo you want to clear all mmult text files (if they exist)?\n"
 				"1 - yes\n"
 				"2 - no\n"
 				"> ");
@@ -37,6 +37,9 @@ int main(void)
 				fclose(fp);
 				
 				fp = fopen("mmult_simd_O3.txt", "w");
+				fclose(fp);
+				
+				fp = fopen("mmult_openMP.txt", "w");
 				fclose(fp);
 				
 				printf("Cleared!\n");
@@ -63,9 +66,10 @@ int main(void)
 		double time = 0;
 		
 		printf("\nPlease select a matrix multiplication option: \n"
-				"1 - w/o SIMD and w/o parallelization (PLEASE ENSURE PROGRAM IS COMPILED W/O -O3 FLAG)\n"
-				"2 - w/ SIMD, non-vectorized (PLEASE ENSURE PROGRAM IS COMPILED W/O -O3 FLAG)\n"
-				"3 - w/ SIMD, vectorized (PLEASE ENSURE PROGRAM IS COMPILED W/ -O3 FLAG)\n"
+				"1 - unoptimized (PLEASE ENSURE PROGRAM IS COMPILED W/O -O3 FLAG)\n"
+				"2 - SIMD (PLEASE ENSURE PROGRAM IS COMPILED W/O -O3 FLAG)\n"
+				"3 - SIMD w/ -O3 (PLEASE ENSURE PROGRAM IS COMPILED W/ -O3 FLAG)\n"
+				"4 - openMP (PLEASE ENSURE PROGRAM IS COMPILED W/O -O3 FLAG)\n"
 				"-1 - End the program\n"
 				"> ");
 		if(scanf("%d", &selection) != 1)
@@ -87,11 +91,11 @@ int main(void)
 					// allocate memory for the result matrix
 					c = malloc(sizeof(double) * n * n);
 					// start timer
-					clock_gettime (CLOCK_REALTIME , &start);
+					clock_gettime(CLOCK_REALTIME , &start);
 					// call mmult - this will multiply a*b and put the result in c
 					mmult(c, a, n, n, b, n, n);
 					// end timer
-					clock_gettime (CLOCK_REALTIME , &end);
+					clock_gettime(CLOCK_REALTIME , &end);
 					// calculate and print the time it took to calculate a*b
 					time = deltaTime(&start, &end);
 					printf("%d %f\n", n, time);
@@ -114,11 +118,11 @@ int main(void)
 					// allocate memory for the result matrix
 					c = malloc(sizeof(double) * n * n);
 					// start timer
-					clock_gettime (CLOCK_REALTIME , &start);
-					// call mmult - this will multiply a*b and put the result in c
+					clock_gettime(CLOCK_REALTIME , &start);
+					// call mmult_simd
 					mmult_simd(c, a, n, n, b, n, n);
 					// end timer
-					clock_gettime (CLOCK_REALTIME , &end);
+					clock_gettime(CLOCK_REALTIME , &end);
 					// calculate and print the time it took to calculate a*b
 					time = deltaTime(&start, &end);
 					printf("%d %f\n", n, time);
@@ -141,11 +145,38 @@ int main(void)
 					// allocate memory for the result matrix
 					c = malloc(sizeof(double) * n * n);
 					// start timer
-					clock_gettime (CLOCK_REALTIME , &start);
-					// call mmult - this will multiply a*b and put the result in c
-					mmult_simd(c, a, n, n, b, n, n);
+					clock_gettime(CLOCK_REALTIME , &start);
+					// call mmult_simd_O3
+					mmult_simd_O3(c, a, n, n, b, n, n);
 					// end timer
-					clock_gettime (CLOCK_REALTIME , &end);
+					clock_gettime(CLOCK_REALTIME , &end);
+					// calculate and print the time it took to calculate a*b
+					time = deltaTime(&start, &end);
+					printf("%d %f\n", n, time);
+					
+					fprintf(fp, "%d %f\n", n, time);
+					fclose(fp);
+				}
+				
+				free(c);
+			}
+			else if(selection == 4)
+			{
+				for(; n <= 1000; n += 100)
+				{
+					fp = fopen("mmult_openMP.txt", "a");
+					
+					// gen_matrix returns an n*n matrix
+					a = gen_matrix (n, n);
+					b = gen_matrix (n, n);
+					// allocate memory for the result matrix
+					c = malloc(sizeof(double) * n * n);
+					// start timer
+					clock_gettime (CLOCK_REALTIME , &start);
+					// call mmult_omp
+					mmult_omp(c, a, n, n, b, n, n);
+					// end timer
+					clock_gettime(CLOCK_REALTIME , &end);
 					// calculate and print the time it took to calculate a*b
 					time = deltaTime(&start, &end);
 					printf("%d %f\n", n, time);
